20bd0b0dbe27dcc4db9903d995480e84ac425cd7
Tupryk <eck4art@gmail.com>
2023-12-11 10:47:07 +0100

    Really good point clound normal calculation

--- RandomPushingV2/PointClouds/Downsampling.py
+++ RandomPushingV2/PointClouds/Downsampling.py
@@ -18,6 +18,7 @@ def pointNormalVoxels(pointCloud, voxelSize=.005):
     # Prepare Voxels
     dims = max_coor-min_coor
     voxelSpaceDims = np.ceil(dims/voxelSize)
+    voxelSpaceDims = voxelSpaceDims.astype(int)
     voxels = [
         [[[] for _ in range(voxelSpaceDims[2])]
         for _ in range(voxelSpaceDims[1])]
@@ -32,6 +33,27 @@ def pointNormalVoxels(pointCloud, voxelSize=.005):
         y = y if y != voxelSpaceDims[1] else voxelSpaceDims[1]-1
         z = int(((point[2] - min_coor[2])/ dims[2]) * voxelSpaceDims[2])
         z = z if z != voxelSpaceDims[2] else voxelSpaceDims[2]-1
-        voxels[x][y][z].append([np.array(point), pointCloud.normals[i]])
+        voxels[x][y][z].append([point, pointCloud.normals[i]])
 
-    return voxels
+    return voxels, voxelSpaceDims
+
+def normalVoting(normals, outlierThresh=3.):
+    # Outlier detection
+    mean = np.mean(normals, axis=0)
+    std_dev = np.std(normals, axis=0)
+
+    # Avoid invalid divisions
+    epsilon = 1e-10
+    std_dev += epsilon
+
+    # Remove outliers
+    z_scores = np.abs((normals - mean) / std_dev)
+    outlier_mask = np.any(z_scores > outlierThresh, axis=1)
+    if len(outlier_mask) != len(normals):
+        filtered_points = normals[~outlier_mask]
+    else: filtered_points = normals
+
+    # Mean of remaining points
+    mean_normal = np.mean(filtered_points, axis=0)
+    mean_normal /= np.linalg.norm(mean_normal)
+    return mean_normal
