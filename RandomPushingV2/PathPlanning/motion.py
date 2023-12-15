70ccf9ddcae5e5792f2bda17e92db4aa5d1ab435
Tupryk <eck4art@gmail.com>
2023-12-15 10:42:56 +0100

    per point pushing now works nicely

--- RandomPushingV2/PathPlanning/motion.py
+++ RandomPushingV2/PathPlanning/motion.py
@@ -2,10 +2,33 @@ import numpy as np
 import robotic as ry
 
 
-def pushPCpoint(point, normal, robot_pos):
+def pushPCpoint(point, normal, robot_pos, start_dist=.15, end_dist_range=[.1, .15], initial_elevation=.15, config=None):
+
     robot2point = point-robot_pos
-    start_vec = normal * (-.05 if np.inner(robot2point, normal) < 0 else .05)
-    waypoints = [point-start_vec, point, point+(np.random.rand()*.04+.01)*normal]
+    dir = -1. if np.inner(robot2point, normal) < 0 else 1.
+    start_pos = normal * start_dist * dir
+    end_dist = np.random.rand() * (end_dist_range[1]-end_dist_range[0]) + end_dist_range[0]
+    end_pos = normal * end_dist * dir
+    initial = point.copy()+start_pos.copy()
+    initial[2] += initial_elevation
+    waypoints = [initial, point+start_pos, point, point-end_pos]
+    
+    if config:
+        for i, w in enumerate(waypoints):
+            way = config.getFrame(f"way{i}")
+
+            if not way:
+                way = config.addFrame(f"way{i}") \
+                .setShape(ry.ST.marker, size=[.1])
+                if i == 0:
+                    way.setColor([1, 0, 0])
+                elif i == len(waypoints)-1:
+                    way.setColor([0, 0, 1])
+                else:
+                    way.setColor([0, 1, 0])
+
+            way.setPosition(w)
+
     return waypoints
 
 def computeKomo(C, ways, verbose=0):
@@ -15,14 +38,17 @@ def computeKomo(C, ways, verbose=0):
 
     komo.setTiming(len(ways), 3, 1., 2)
 
-    move_dir = ways[-1]-ways[0]
+    move_dir = ways[-1]-ways[1]
     move_dir /= np.linalg.norm(move_dir)
 
-    # komo.addObjective([], ry.FS.accumulatedCollisions, [], ry.OT.eq)
-    # komo.addObjective([], ry.FS.jointLimits, [], ry.OT.ineq)
+    komo.addControlObjective([], 0, 1e-2)
+    komo.addControlObjective([], 2, 1e1)
 
-    komo.addObjective([], ry.FS.vectorZ, ['l_gripper'], ry.OT.eq, [1e1], move_dir)
-    # komo.addObjective([], ry.FS.vectorX, ['l_gripper'], ry.OT.eq, [1e1], [1., 0., 0.])
+    # komo.addObjective([], ry.FS.accumulatedCollisions, [], ry.OT.eq)
+    komo.addObjective([], ry.FS.jointLimits, [], ry.OT.ineq)
+
+    komo.addObjective([], ry.FS.vectorZ, ['l_gripper'], ry.OT.eq, [1e1], -move_dir)
+    komo.addObjective([], ry.FS.scalarProductXZ, ['l_gripper', 'table'], ry.OT.eq, [1e1], [0.])
     for i, way in enumerate(ways):
         komo.addObjective([i+1.], ry.FS.position, ['l_gripper'], ry.OT.eq, [1e1], way)
 
