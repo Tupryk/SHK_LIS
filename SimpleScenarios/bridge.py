7f9a281f0296b0f835142a1cf12fb5748d8acae6
Denis Shcherba <d.e.shcherba@gmail.com>
2023-12-29 14:13:48 +0100

    works (kinda) bad though)

--- SimpleScenarios/bridge.py
+++ SimpleScenarios/bridge.py
@@ -42,6 +42,12 @@ C.addFrame('block2way2'). setShape(ry.ST.marker, [.1]) .setPosition([.15,.12, .6
 C.addFrame('block2way3'). setShape(ry.ST.marker, [.1]) .setPosition([.15,.12, .67+dropoffHeight-.09])
 C.addFrame('block2way4'). setShape(ry.ST.marker, [.1]) .setPosition([.0,.12, .67+dropoffHeight-.09])
 
+#TODO
+C.addFrame('block3way1'). setShape(ry.ST.marker, [.1]) .setPosition([-.25, .12, .67+dropoffHeight])
+C.addFrame('block3way2'). setShape(ry.ST.marker, [.1]) .setPosition([.20,.12, .67+dropoffHeight+.03])
+C.addFrame('block3way3'). setShape(ry.ST.marker, [.1]) .setPosition([.20,.12, .67+dropoffHeight])
+C.addFrame('block3way4'). setShape(ry.ST.marker, [.1]) .setPosition([.0,.12, .67+dropoffHeight-.09])
+
 
 def openGripper(C, width=.075, speed=.2):
     bot.gripperMove(ry._left, width=width, speed=speed)
@@ -103,16 +109,17 @@ def grabBlock(C, bot, block):
     while not bot.gripperDone(ry._left):
         bot.sync(C, .1)
 
-def maneuverTo(C, bot, block):
+def maneuverTo(C, bot, block, position="orthogonal"):
     #Get to waypint then after
     komo = ry.KOMO(C, phases=4, slicesPerPhase=1, kOrder=1, enableCollisions=True)
     komo.addControlObjective([], 0, 1e-1)
     komo.addControlObjective([], 1, 1e0)
     komo.addObjective([1], ry.FS.positionDiff, ['l_gripper', f'{block}way1'], ry.OT.eq, [1e1])
     komo.addObjective([2], ry.FS.positionDiff, ['l_gripper', f'{block}way2'], ry.OT.eq, [1e1])
-    komo.addObjective([2], ry.FS.scalarProductYZ, ['l_gripper', f'{block}way2'], ry.OT.eq, [1e1], [1])
+    if(position=="orthogonal"):
+        komo.addObjective([2], ry.FS.scalarProductYZ, ['l_gripper', f'{block}way2'], ry.OT.eq, [1e1], [1])
     komo.addObjective([3], ry.FS.positionDiff, ['l_gripper', f'{block}way3'], ry.OT.eq, [1e1])
-    komo.addObjective([4], ry.FS.positionDiff, ['l_gripper', f'{block}way4'], ry.OT.eq, [1e1], [.1])
+    komo.addObjective([4], ry.FS.positionRel, [f'{block}way3', "cameraWrist"], ry.OT.eq, [1.], [.0, .0, .2])
 
 
     ret = ry.NLP_Solver(komo.nlp(), verbose=0 ) .solve()
@@ -125,7 +132,7 @@ def maneuverTo(C, bot, block):
     print(q)
     C.setJointState(q[0])
 
-    komo.view(True)
+    #komo.view(True)
     bot.move(q[:-1], [1,3,4.5])  # save last pathstep for after loslassing
 
     while bot.getTimeToEnd()>0:
@@ -133,7 +140,7 @@ def maneuverTo(C, bot, block):
 
     openGripper(C)
 
-    bot.move(np.asarray([C.getJointState(), q[-1]]), [2])  # save last pathstep for after loslassing
+    bot.move(np.asarray([C.getJointState(), q[-1]]), [1])  # save last pathstep for after loslassing
 
     while bot.getTimeToEnd()>0:
         key = bot.sync(C, .1)
@@ -156,5 +163,8 @@ bot.home(C)
 
 grabBlock(C, bot, "block2")
 maneuverTo(C, bot, "block2")
-openGripper(C)
 bot.home(C)
+
+grabBlock(C, bot, "block3")
+maneuverTo(C, bot, "block3", "parallel")
+
