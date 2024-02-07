ed20f394698d1bfc99227afd325d4be4c6960468
Denis Shcherba <31022562+denis-shcherba@users.noreply.github.com>
2024-02-07 17:12:46 +0100

    first basic icp version on cpp

--- PointCloudProcessing/iterative_closest_point.cpp
+++ PointCloudProcessing/iterative_closest_point.cpp
@@ -2,55 +2,49 @@
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 #include <pcl/registration/icp.h>
+#include <pcl/visualization/cloud_viewer.h>
+
 
 int main()
 {
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in(new pcl::PointCloud<pcl::PointXYZ>(5, 1));
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_out(new pcl::PointCloud<pcl::PointXYZ>);
+    // Load point cloud data from files
+    std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> clouds;
 
-    // Fill in the CloudIn data
-    for (auto &point : *cloud_in)
+    // Load point cloud data from files
+    for (int i = 0; i < 8; ++i)
     {
-        point.x = 1024 * rand() / (RAND_MAX + 1.0f);
-        point.y = 1024 * rand() / (RAND_MAX + 1.0f);
-        point.z = 1024 * rand() / (RAND_MAX + 1.0f);
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+        std::string filename = "../data/point_cloud_" + std::to_string(i) + ".pcd";
+        if (pcl::io::loadPCDFile<pcl::PointXYZ>(filename, *cloud) == -1)
+        {
+            PCL_ERROR("Couldn't read file %s \n", filename.c_str());
+            return (-1);
+        }
+        std::cout << "Loaded " << cloud->size() << " data points from " << filename << std::endl;
+        clouds.push_back(cloud);
     }
 
-    std::cout << "Saved " << cloud_in->size() << " data points to input:" << std::endl;
 
-    for (auto &point : *cloud_in)
+    pcl::PointCloud<pcl::PointXYZ>::Ptr result_cloud(new pcl::PointCloud<pcl::PointXYZ>); // Initialize result cloud with the first input cloud
+    *result_cloud = *clouds[0];
+
+    // Perform ICP registration for each pair of consecutive point clouds
+    for (size_t i = 1; i < clouds.size(); ++i)
     {
-        std::cout << point << std::endl;
+        pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
+        icp.setInputSource(clouds[i]);
+        icp.setInputTarget(result_cloud);
+
+        pcl::PointCloud<pcl::PointXYZ>::Ptr aligned_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+        icp.align(*aligned_cloud);
+
+        std::cout << "Pair " << i << " registration has converged: " << icp.hasConverged()
+                  << " score: " << icp.getFitnessScore() << std::endl;
+
+        *result_cloud += *aligned_cloud; // Merge aligned_cloud with result_cloud
     }
 
-    *cloud_out = *cloud_in;
-
-    std::cout << "size:" << cloud_out->size() << std::endl;
-
-    for (auto &point : *cloud_out)
-    {
-        point.x += 0.7f;
-    }
-
-    std::cout << "Transformed " << cloud_in->size() << " data points:" << std::endl;
-
-    for (auto &point : *cloud_out)
-    {
-        std::cout << point << std::endl;
-    }
-
-    pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
-
-    icp.setInputSource(cloud_in);
-    icp.setInputTarget(cloud_out);
-
-    pcl::PointCloud<pcl::PointXYZ> Final;
-
-    icp.align(Final);
-
-    std::cout << "has converged:" << icp.hasConverged() << " score: " << icp.getFitnessScore() << std::endl;
-
-    std::cout << icp.getFinalTransformation() << std::endl;
+    pcl::io::savePCDFileASCII("../data/result_cloud.pcd", *result_cloud);
 
     return (0);
 }
