fa4e780fd4e3c86848893d45a604f8a3580d3eac
Denis Shcherba <d.e.shcherba@gmail.com>
2024-04-09 13:29:06 +0200

    changed one gripper

--- StaticScanner/dualGrasp.py
+++ StaticScanner/dualGrasp.py
@@ -2,6 +2,7 @@ import robotic as ry
 import matplotlib.pyplot as plt
 import numpy as np
 from helperFunctions import filter_points_outside_cuboid, generate_path_on_partial_spherical_surface
+import open3d as o3d 
 
 ON_REAL = False
 
@@ -28,12 +29,13 @@ def main():
 
     komo = ry.KOMO()
     komo.setConfig(C, True)
-    komo.setTiming(1., 1, 1., 0)
+    komo.setTiming(len(sphere_path), 1, 1., 0)
     komo.addObjective([], ry.FS.accumulatedCollisions, [], ry.OT.eq)
     komo.addObjective([], ry.FS.jointLimits, [], ry.OT.ineq)
 
-    komo.addObjective([1.], ry.FS.positionRel, ["predicted_obj", "wristCam"], ry.OT.eq, [1.], [.0, .0, .4])
-    komo.addObjective([1.], ry.FS.position, ["wristCam"], ry.OT.eq, [1.], sphere_path[0])
+    for i, point in enumerate(sphere_path):
+        komo.addObjective([i+1], ry.FS.positionRel, ["predicted_obj", "wristCam"], ry.OT.eq, [1.], [.0, .0, .4])
+        komo.addObjective([i+1], ry.FS.position, ["wristCam"], ry.OT.eq, [1.], point)
 
     komo.addObjective([], ry.FS.qItself, [], ry.OT.sos, [.1], q_home)
     komo.addObjective([], ry.FS.qItself, [], ry.OT.sos, [.1], q_now)
@@ -45,51 +47,54 @@ def main():
     
     print(ret)
         
-    bot.moveTo(komo.getPath()[0], 1., False)
-    while bot.getTimeToEnd() > 0:
-        key=bot.sync(C, .1)
-        if chr(key) == "q":
-            print("Terminated (visual)")
-            bot.home(C)
-            del bot
-            del C
-            exit()
 
-    _, __, points = bot.getImageDepthPcl('wristCam', False)
-    cameraFrame = C.getFrame("wristCam")
-    R, t = cameraFrame.getRotationMatrix(), cameraFrame.getPosition()
+    for i, _ in enumerate(sphere_path):
+        bot.moveTo(komo.getPath()[i], 1., False)
+        while bot.getTimeToEnd() > 0:
+            key=bot.sync(C, .1)
+            if chr(key) == "q":
+                print("Terminated (visual)")
+                bot.home(C)
+                del bot
+                del C
+                exit()
 
-    # points to world coords and to N x 3
-    points = points.reshape(points.shape[0]*points.shape[1],points.shape[2])
-    points = points @ R.T
-    points = points + np.tile(t.T, (points.shape[0], 1))
+        _, __, points = bot.getImageDepthPcl('wristCam', False)
+        cameraFrame = C.getFrame("wristCam")
+        R, t = cameraFrame.getRotationMatrix(), cameraFrame.getPosition()
 
-    # filter points on the table and from robot base  
-    #points = filter_points_outside_cuboid(points, (0, -.225, .75), (.275, .275, .4))   TODO 2 times for both robot arms
-    filtered_points = points[points[:, 2] >= .609]+ np.array([.3, 0,0])
+        # points to world coords and to N x 3
+        points = points.reshape(points.shape[0]*points.shape[1],points.shape[2])
+        points = points @ R.T
+        points = points + np.tile(t.T, (points.shape[0], 1))
+        points = points[points[:, 2] >= .609]
+
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(points)
+        # Save the point cloud as a PCD file
+        o3d.io.write_point_cloud(f"data/giraffe/point_cloud_{i}.pcd", pcd)
+        # filter points on the table and from robot base  
+        #points = filter_points_outside_cuboid(points, (0, -.225, .75), (.275, .275, .4))   TODO 2 times for both robot arms
+        filtered_points = points+ np.array([.3, 0,0])
+        
+        pclFrame = C.addFrame("pcl")
+
+        pclFrame.setPointCloud(filtered_points)
+        C.view_recopyMeshes()
+        C.view(True)
     
-    pclFrame = C.addFrame("pcl")
 
-    pclFrame.setPointCloud(filtered_points)
-    C.view_recopyMeshes()
-    C.view(True)
-
-
-    
-    
-    
-    # NOW BS TODO
+    #experimental 
     q_now = C.getJointState()
     q_home = bot.get_qHome()
 
     komo = ry.KOMO()
     komo.setConfig(C, True)
-    komo.setTiming(1., 1, 1., 0)
+    komo.setTiming(1, 1, 1., 0)
     komo.addObjective([], ry.FS.accumulatedCollisions, [], ry.OT.eq)
     komo.addObjective([], ry.FS.jointLimits, [], ry.OT.ineq)
 
-    komo.addObjective([1.], ry.FS.positionRel, ["predicted_obj", "wristCam"], ry.OT.eq, [1.], [.0, .0, .4])
-    komo.addObjective([1.], ry.FS.position, ["wristCam"], ry.OT.eq, [1.], sphere_path[1])
+    komo.addObjective([1], ry.FS.position, ["l_gripper"], ry.OT.eq, [1.], C.getFrame("predicted_obj").getPosition())
 
     komo.addObjective([], ry.FS.qItself, [], ry.OT.sos, [.1], q_home)
     komo.addObjective([], ry.FS.qItself, [], ry.OT.sos, [.1], q_now)
@@ -99,8 +104,6 @@ def main():
         .setOptions(stopTolerance=1e-2, verbose=0) \
         .solve()
     
-    print(ret)
-        
     bot.moveTo(komo.getPath()[0], 1., False)
     while bot.getTimeToEnd() > 0:
         key=bot.sync(C, .1)
@@ -110,23 +113,6 @@ def main():
             del bot
             del C
             exit()
-
-    _, __, points = bot.getImageDepthPcl('wristCam', False)
-    cameraFrame = C.getFrame("wristCam")
-    R, t = cameraFrame.getRotationMatrix(), cameraFrame.getPosition()
-
-    # points to world coords and to N x 3
-    points = points.reshape(points.shape[0]*points.shape[1],points.shape[2])
-    points = points @ R.T
-    points = points + np.tile(t.T, (points.shape[0], 1))
-
-    # filter points on the table and from robot base  
-    #points = filter_points_outside_cuboid(points, (0, -.225, .75), (.275, .275, .4))   TODO 2 times for both robot arms
-    filtered_points = np.concatenate([filtered_points, (points[points[:, 2] >= .609]+ np.array([.3, 0,0]))])
-    
-    pclFrame.setPointCloud(filtered_points)
-    C.view_recopyMeshes()
-    C.view(True)
 
 
 if __name__ == "__main__":
