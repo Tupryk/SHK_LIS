b6ada27b5e3fb81d3fd9bf69d56ce31800fac475
Eckart Cobo-Briesewitz <68149355+Tupryk@users.noreply.github.com>
2024-04-25 17:09:18 +0200

    updated teleop code

--- Teleoperation/main.cpp
+++ Teleoperation/main.cpp
@@ -5,17 +5,19 @@
 #include <BotOp/bot.h>
 #include <Optim/NLP_Solver.h>
 
-#define BOTH_ARMS 1
+#define BOTH_ARMS 0
 
 
-void reload_target(rai::Configuration* C, arr target_origin, arr controller_origin, const char* controller, const char* gripper_target) {
+void reload_target(rai::Configuration* C, arr target_origin, arr controller_origin, arr rotation_offset, const char* controller, const char* gripper_target) {
     arr controller_quat = C->getFrame(controller)->getQuaternion();
-    controller_quat.elem(1) *= -1;
-    controller_quat.elem(2) *= -1;
+    auto tmp_quat = controller_quat.elem(1);
+    controller_quat.elem(1) = -controller_quat.elem(2);
+    controller_quat.elem(2) = tmp_quat;
     C->getFrame(gripper_target)->setQuaternion(controller_quat);
     arr controller_pos = C->getFrame(controller)->getPosition() - controller_origin;
-    controller_pos.elem(0) *= -1;
-    controller_pos.elem(1) *= -1;
+    auto tmp_pos = controller_pos.elem(0);
+    controller_pos.elem(0) = -controller_pos.elem(1);
+    controller_pos.elem(1) = tmp_pos;
     arr target_pos = target_origin + controller_pos*2;
     C->getFrame(gripper_target)->setPosition(target_pos);
 }
@@ -24,17 +26,22 @@ int main(int argc,char **argv) {
     rai::initCmdLine(argc, argv);
 
     rai::Configuration C;
-    C.addFile(rai::raiPath("../rai-robotModels/scenarios/pandasTable.g"));
-
-    rai::OptiTrack OT;
-
-    OT.pull(C);
+    C.addFile(rai::raiPath("../rai-robotModels/scenarios/pandaSingle.g"));
     C.view(false);
-
-    bool activated = false;
 
     BotOp bot(C, true);
     bot.home(C);
+    bot.gripperMove(rai::_left, .079);
+    #if BOTH_ARMS
+    bot.gripperMove(rai::_right, .079);
+    #endif
+
+    rai::OptiTrack OT;
+    OT.pull(C);
+
+    bool activated = false;
+    bool l_gripper_closed = false;
+    // bool r_gripper_closed = false;
 
     #if BOTH_ARMS
     arr r_controller_origin = arr{0., 0., 0.};
@@ -70,30 +77,33 @@ int main(int argc,char **argv) {
         if (activated) {
 
             #if BOTH_ARMS
-            reload_target(&C, r_target_origin, r_controller_origin, "r_controller", "r_gripper_target");
+            reload_target(&C, r_target_origin, r_controller_origin, r_rotation_offset, "r_controller", "r_gripper_target");
             #endif
-            reload_target(&C, l_target_origin, l_controller_origin, "l_controller", "l_gripper_target");
+            reload_target(&C, l_target_origin, l_controller_origin, l_rotation_offset, "l_controller", "l_gripper_target");
 
             KOMO komo(C, 1., 1, 2, true);
-            komo.addControlObjective({}, 0, 1.);
+            komo.addControlObjective({}, 0, 1e1);
             komo.addControlObjective({}, 1, .1);
             komo.addControlObjective({}, 2, .1);
             komo.addObjective({}, FS_accumulatedCollisions, {}, OT_eq, {1e10});
+            komo.addObjective({}, FS_jointLimits, {}, OT_ineq, {1e0});
 
-            bool should_translate = -C.eval(FS_negDistance, {"l_gripper", "l_gripper_target"}).elem(0) > .01;
-            if (should_translate) {
+            bool l_should_translate = -C.eval(FS_negDistance, {"l_gripper", "l_gripper_target"}).elem(0) > .01;
+            if (l_should_translate) {
                 komo.addObjective({1.}, FS_positionDiff, {"l_gripper", "l_gripper_target"}, OT_eq, {1e1});
             } else {
                 komo.addObjective({}, FS_position, {"l_gripper"}, OT_eq, {1e1}, C.getFrame("l_gripper")->getPosition());
             }
 
             #if BOTH_ARMS
-            should_translate = -C.eval(FS_negDistance, {"r_gripper", "r_gripper_target"}).elem(0) > .01;
-            if (should_translate) {
+            bool r_should_translate = -C.eval(FS_negDistance, {"r_gripper", "r_gripper_target"}).elem(0) > .01;
+            if (r_should_translate) {
                 komo.addObjective({1.}, FS_positionDiff, {"r_gripper", "r_gripper_target"}, OT_eq, {1e1});
             } else {
                 komo.addObjective({}, FS_position, {"r_gripper"}, OT_eq, {1e1}, C.getFrame("r_gripper")->getPosition());
             }
+            #else
+            bool r_should_translate = false;
             #endif
 
             #if BOTH_ARMS
@@ -101,7 +111,7 @@ int main(int argc,char **argv) {
             #endif
             komo.addObjective({1.}, FS_quaternionDiff, {"l_gripper", "l_gripper_target"}, OT_eq, {1e1});
 
-            if (should_translate) {
+            if (r_should_translate || l_should_translate) {
                 auto ret = NLP_Solver()
                     .setProblem(komo.nlp())
                     .solve();
@@ -110,6 +120,37 @@ int main(int argc,char **argv) {
                 bot.moveTo(q[0], {5.}, true);
                 bot.sync(C, 0);
             }
+
+            float l_gripper_pos = .079 - (C.eval(FS_negDistance, {"l_controller", "l_thumb"}).elem(0)+.014) / -.07 * .079;
+            if (l_gripper_pos > .079) l_gripper_pos = .079;
+            if (l_gripper_pos < .0) l_gripper_pos = .0;
+            std::cout << "Left gripper pos: " << l_gripper_pos << std::endl;
+
+            // bot.gripperMove(rai::_left, l_gripper_pos);
+            if (l_gripper_closed && l_gripper_pos >= .079*.66) {
+                bot.gripperMove(rai::_left, .079);
+                l_gripper_closed = false;
+            }
+            else if (!l_gripper_closed && l_gripper_pos <= .079*.33) {
+                bot.gripperMove(rai::_left, .0);
+                l_gripper_closed = true;
+            }
+
+            #if BOTH_ARMS
+            float r_gripper_pos = .079 - (C.eval(FS_negDistance, {"r_controller", "r_thumb"}).elem(0)+.014) / -.07 * .079;
+            if (r_gripper_pos > .079) r_gripper_pos = .079;
+            if (r_gripper_pos < .0) r_gripper_pos = .0;
+            std::cout << "Right gripper pos: " << r_gripper_pos << std::endl;
+
+            if (r_gripper_closed && r_gripper_pos >= .079*.66) {
+                bot.gripperMove(rai::_right, .079);
+                l_gripper_closed = false;
+            }
+            else if (!r_gripper_closed && r_gripper_pos <= .079*.33) {
+                bot.gripperClose(rai::_right);
+                r_gripper_closed = true;
+            }
+            #endif
         }
     }
 
