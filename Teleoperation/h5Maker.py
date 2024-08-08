c1f98220774bf37eb8b95e19483155508ccf0baf
Denis Shcherba <31022562+denis-shcherba@users.noreply.github.com>
2024-08-08 14:14:02 +0200

    now for all subdirs (maybe)?

--- Teleoperation/h5Maker.py
+++ Teleoperation/h5Maker.py
@@ -4,81 +4,87 @@ import os
 from PIL import Image
 import numpy as np
 
-file_path = '/home/eckart/SHK_LIS/Teleoperation/recordings_new/proprioceptive.txt'
-
-qpos_data = []
-tau_external_data = []
-last_number = []
-
-with open(file_path, 'r') as file:
-    for line in file:
-        parts = line.split('] [')
-        
-        first_part = parts[0].strip('[').strip()
-        second_part = parts[1].split('] ')[0].strip()
-        last_part = parts[1].split('] ')[1].strip()
-        
-        first_list = ast.literal_eval(f'[{first_part}]')
-        second_list = ast.literal_eval(f'[{second_part}]')
-        last_value = int(last_part)
-        first_list.append(last_value)
-
-        qpos_data.append(first_list)
-        tau_external_data.append(second_list)
-
-print("First Column:")
-print(qpos_data)
-print("\nSecond Column:")
-print(tau_external_data)
+# Set the path to the main directory containing subdirectories
+main_directory = '/home/eckart/SHK_LIS/Teleoperation/recordings/'
 
 
-import os
-from PIL import Image
-import numpy as np
+def process_proprioceptive_data(file_path):
+    qpos_data = []
+    tau_external_data = []
+    
+    with open(file_path, 'r') as file:
+        for line in file:
+            parts = line.split('] [')
 
-folder_path = 'recordings_new/images'
+            first_part = parts[0].strip('[').strip()
+            second_part = parts[1].split('] ')[0].strip()
+            last_part = parts[1].split('] ')[1].strip()
 
-cam1_images = []
-cam2_images = []
+            first_list = ast.literal_eval(f'[{first_part}]')
+            second_list = ast.literal_eval(f'[{second_part}]')
+            last_value = int(last_part)
+            first_list.append(last_value)
 
-for filename in os.listdir(folder_path):
-    if filename.endswith('.jpg') or filename.endswith('.png'):  # Modify the extensions as needed
-        if filename.startswith('cam1'):
+            qpos_data.append(first_list)
+            tau_external_data.append(second_list)
+
+    return qpos_data, tau_external_data
+
+# Function to process images from a folder
+def process_images(folder_path):
+    cam1_images = []
+    cam2_images = []
+
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.jpg') or filename.endswith('.png'):  # Modify the extensions as needed
             image_path = os.path.join(folder_path, filename)
             image = Image.open(image_path).convert('RGB')  # Ensure image is in RGB mode
-
             image_array = np.array(image)
 
-            cam1_images.append(image_array)
+            if filename.startswith('cam1'):
+                cam1_images.append(image_array)
+            elif filename.startswith('cam2'):
+                cam2_images.append(image_array)
+
+    return cam1_images, cam2_images
+
+
+def save_hdf5(filename, qpos_data, tau_external_data, cam1_images, cam2_images):
+    with h5py.File(filename, "w") as hdf:
+        hdf.attrs["sim"] = False
         
-        elif filename.startswith('cam2'):
-            image_path = os.path.join(folder_path, filename)
-            image = Image.open(image_path).convert('RGB')  # Ensure image is in RGB mode
+        hdf.create_dataset("/action", data=qpos_data[1:])
+        
+        observations_group = hdf.create_group("/observations")
+        observations_group.create_dataset("qpos", data=qpos_data)
+        observations_group.create_dataset("qvel", data=[])
+        observations_group.create_dataset("tau", data=tau_external_data)
 
-            image_array = np.array(image)
-
-            cam2_images.append(image_array)
-
-print(f'Number of cam1 images: {len(cam1_images)}')
-print(f'Number of cam2 images: {len(cam2_images)}')
-print(f'Number of tau ext: {len(tau_external_data)}')
-print(f'Number of qpos: {len(qpos_data)}')
-
-
+        images_group = observations_group.create_group("images")
+        images_group.create_dataset("cam1", data=cam1_images)
+        images_group.create_dataset("cam2", data=cam2_images)
+        
+        hdf.create_dataset("/waypoints", data=[])
 
 
-with h5py.File(f"episode_1.hdf5", "w") as hdf:
-    hdf.attrs["sim"] = False
+for subdir in os.listdir(main_directory):
+    subdir_path = os.path.join(main_directory, subdir)
     
-    hdf.create_dataset("/action", data=qpos_data[1:])
-    
-    observations_group = hdf.create_group("/observations")
-    observations_group.create_dataset("qpos", data=qpos_data)
-    observations_group.create_dataset("qvel", data=[])
-    observations_group.create_dataset("tau", data=tau_external_data)
+    if os.path.isdir(subdir_path):
+        data_folder_path = os.path.join(subdir_path, 'images')
+        print(f"Processing data folder: {data_folder_path}")
+        
+        # Process proprioceptive.txt
+        txt_file_path = os.path.join(data_folder_path, 'proprioceptive.txt')
+        qpos_data, tau_external_data = process_proprioceptive_data(txt_file_path)
+        
+        # Process images
+        images_folder_path = os.path.join(data_folder_path, 'images')
+        cam1_images, cam2_images = process_images(images_folder_path)
+        
+        # Save to HDF5
+        output_hdf5_file = os.path.join(main_directory, f"{subdir}.hdf5")
+        save_hdf5(output_hdf5_file, qpos_data, tau_external_data, cam1_images, cam2_images)
+        
+        print(f"Saved HDF5 file: {output_hdf5_file}")
 
-    images_group = observations_group.create_group("images")
-    images_group.create_dataset("cam1", data=cam1_images)
-    images_group.create_dataset("cam2", data=cam2_images)
-    
-    hdf.create_dataset("/waypoints", data=[])
